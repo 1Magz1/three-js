ab5d3ea03696753b32b5d4936d92f0e016033181
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-04 16:50:44 +0600

    add space scene component

---
+++ src/components/SpaceScene.vue
@@ -0,0 +1,13 @@
+<template>
+  <canvas id="space-scene" />
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import SpaceScene from '../webgl/SpaceScene';
+
+onMounted(() => {
+  const webgl = new SpaceScene();
+  webgl.create();
+});
+</script>
