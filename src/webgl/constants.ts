37bdbad4bc01526c54b245f7fe1cc3a7a08cb8c7
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-04 19:30:14 +0600

    update img

--- src/webgl/constants.ts
+++ src/webgl/constants.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const SPACE_SCENE_TEXTURE_MAP = [
+  'src/assets/textures/sun1.jpg',
   'src/assets/textures/space1.jpg',
-  'src/assets/textures/space2.jpg',
   'src/assets/textures/star1.png',
   'src/assets/textures/star2.png',
   'src/assets/textures/star3.png',
