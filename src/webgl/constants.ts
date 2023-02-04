c9ec0d45f11eba8b32ae91d5ce56c1afc0b31d6e
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-04 19:06:42 +0600

    add img

--- src/webgl/constants.ts
+++ src/webgl/constants.ts
@@ -3,4 +3,7 @@ export const SPACE_SCENE_TEXTURE_MAP = [
   'src/assets/textures/space1.jpg',
   'src/assets/textures/space2.jpg',
   'src/assets/textures/star1.png',
+  'src/assets/textures/star2.png',
+  'src/assets/textures/star3.png',
+  'src/assets/textures/star4.png',
 ];
