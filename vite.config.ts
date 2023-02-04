3f65cf1f8b47760b7b6ec767cb986519b27b4c60
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-04 16:48:41 +0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import stylelint from 'vite-plugin-stylelint';
 import inspect from 'vite-plugin-inspect';
-/* eslint-enabled import/no-extraneous-dependencies */
 
 // https://vitejs.dev/config/
 export default defineConfig({
