3aa8ae7f13228fbfd8691444aa5ca4c1f904c2b1
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-28 16:52:32 +0600

    develop: add sceneList constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+import SpaceScene from '../components/SpaceScene.vue';
+import SynthWaveScene from '../components/SynthWaveScene.vue';
+
+// eslint-disable-next-line import/prefer-default-export
+export const sceneList = [SynthWaveScene, SpaceScene];
