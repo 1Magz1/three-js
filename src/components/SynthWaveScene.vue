aebeff3a546ac395be25a36561ef38544e75096e
1Magz1 <maxim.chipurnoff@gmail.com>
2023-02-28 16:52:47 +0600

    develop: init synth wave scene

---
+++ src/components/SynthWaveScene.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="scene">
+    <canvas id="synth-wave-scene" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.scene {
+  width: 100vh;
+  height: 100vh;
+}
+</style>
