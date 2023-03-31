338c5622664401a30d8c443427b81a65f169e70d
1Magz1 <maxim.chipurnoff@gmail.com>
2023-03-31 20:09:44 +0600

    develop: add lights

--- src/webgl/SynthWaveScene.ts
+++ src/webgl/SynthWaveScene.ts
@@ -1,9 +1,12 @@
 import {
+  DirectionalLight,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
 } from 'three';
 import * as THREE from 'three';
+import * as vertexShader from '../glsl/sunVertexShader.glsl';
+import * as fragmentShader from '../glsl/sunFragmentShader.glsl';
 import AbstractWebgl from './AbstractWebgl';
 
 export default class SynthWaveScene extends AbstractWebgl {
@@ -17,13 +20,17 @@ export default class SynthWaveScene extends AbstractWebgl {
 
   protected scene: Scene | null;
 
+  private directionLight1: DirectionalLight | null;
+
+  private directionLight2: DirectionalLight | null;
+
   constructor(elementId: string) {
     super();
 
     this.canvas = document.getElementById(elementId) as HTMLCanvasElement;
     this.render = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      antialias: false,
+      antialias: true,
       alpha: false,
     });
     this.camera = new THREE.PerspectiveCamera(
@@ -36,10 +43,14 @@ export default class SynthWaveScene extends AbstractWebgl {
     this.animId = requestAnimationFrame((time: number) => {
       this.update(time);
     });
+
+    this.directionLight1 = null;
+    this.directionLight2 = null;
   }
 
   create(): void {
     this.initRender();
+    // this.addSun();
   }
 
   destroy(): void {
@@ -47,6 +58,8 @@ export default class SynthWaveScene extends AbstractWebgl {
     this.render = null;
     this.scene = null;
     this.camera = null;
+    this.directionLight1 = null;
+    this.directionLight2 = null;
 
     if (this.animId) {
       cancelAnimationFrame(this.animId);
@@ -71,17 +84,49 @@ export default class SynthWaveScene extends AbstractWebgl {
   }
 
   protected addLight(): void {
+    this.directionLight1 = new THREE.DirectionalLight(
+      0x2dd7ff,
+      0.85,
+    );
+    this.directionLight1.position.set(15, 1, 5);
+
+    this.directionLight2 = new THREE.DirectionalLight(
+      0x2dd7ff,
+      0.85,
+    );
+    this.directionLight1.position.set(-15, 1, 5);
+
+    this.scene?.add(this.directionLight1);
+    this.scene?.add(this.directionLight2);
+  }
+
+  // Sun
+  private addSun(): void {
+    const geometry = new THREE.SphereGeometry(30, 64, 64);
+    const material = new THREE.ShaderMaterial({
+      // uniforms,
+      vertexShader,
+      fragmentShader,
+      transparent: true,
+    });
+
+    const sun = new THREE.Mesh(geometry, material);
+    sun.position.set(0, 16, -100);
+
+    this.scene?.add(sun);
   }
 
   protected update(time: number): void {
-    console.log(time);
+    if (this.scene && this.camera) {
+      this.render?.render(this.scene, this.camera);
+    }
   }
 
   private initRender(): void {
     this.render?.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     this.render?.setSize(window.innerWidth, window.innerHeight);
 
-    this.camera?.position.set(0, 0, 100);
+    this.camera?.position.set(0, 0, 5);
 
     window.addEventListener('resize', () => {
       this.onWindowResize();
